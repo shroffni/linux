633c9e880cee6b988fca9d281739978e4ba04247
Nilay Shroff <nilay@linux.ibm.com>
2026-02-20 23:18:49 +0530

    nvme: export I/O requeue count when no path is available via sysfs
    When the NVMe namespace head determines that there is no currently
    available path to handle I/O (for example, while a controller is
    resetting/connecting or due to a transient link failure), incoming
    I/Os are added to the requeue list.
    
    Currently, there is no visibility into how many I/Os have been requeued
    in this situation. Add a new ns-head sysfs counter
    io_requeue_no_available_path_count, under diag attribute group to expose
    the number of I/Os that were requeued due to the absence of an available
    path. This counter is also writable thus allowing user to reset it, if
    needed.
    
    This statistic can help users understand I/O slowdowns or stalls caused
    by temporary path unavailability, and can be consumed by monitoring
    tools such as nvme-top for real-time observability.
    
    Signed-off-by: Nilay Shroff <nilay@linux.ibm.com>

--- drivers/nvme/host/sysfs.c
+++ drivers/nvme/host/sysfs.c
@@ -410,6 +410,7 @@ static struct attribute *nvme_ns_diag_attrs[] = {
 	&dev_attr_io_errors.attr,
 #ifdef CONFIG_NVME_MULTIPATH
 	&dev_attr_multipath_failover_count.attr,
+	&dev_attr_io_requeue_no_usable_path_count.attr,
 #endif
 	NULL,
 };
@@ -432,6 +433,10 @@ static umode_t nvme_ns_diag_attrs_are_visible(struct kobject *kobj,
 #ifdef CONFIG_NVME_MULTIPATH
 	if (a == &dev_attr_multipath_failover_count.attr) {
 		if (nvme_disk_is_ns_head(dev_to_disk(dev)))
+			return 0;
+	}
+	if (a == &dev_attr_io_requeue_no_usable_path_count.attr) {
+		if (!nvme_disk_is_ns_head(dev_to_disk(dev)))
 			return 0;
 	}
 #endif
